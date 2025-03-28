9a21d58d404a651472fc22f86ecaab2e90845c24
Dinesh-18-04 <dineshkumars.22cse@kongu.edu>
2025-03-28 12:10:01 +0530

    updated

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import vid from "../assets/videoplayback.webm";
+import vid from "../assets/videoplayback (1).mp4";
 
 const Home = () => {
   return (
